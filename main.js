6314cea7c0a53b73ca606a2e74aa50e7f3d9faf5
x-builder1965 <tsuyo.hasegwa@gmail.com>
2026-03-18 23:31:06 +0900

    Ver3.74 🎤音声トラック・🔠字幕トラックの関連機能追加。（開発中：Step2.5途中）

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------
 const copyright = 'Copyright © 2025 @x-builder, Japan';
 const email = 'x-builder@gmail.com';
-const appName = 'xPlayer -動画プレイヤー- Ver3.72';
+const appName = 'xPlayer -動画プレイヤー- Ver3.74';
 // ---------------------------------------------------------------------
 
 // 🔲共通変数設定🔲
