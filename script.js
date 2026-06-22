76cdb3db35d68f85027ed9b096698c7c8dc530a2
x-builder1965 <tsuyo.hasegwa@gmail.com>
2026-06-22 12:46:35 +0900

    Ver4.32.2 ヘルプ見た目調整。ズームバーのサイズ調整。（Ver修正）

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------
 const copyright = 'Copyright © 2025- @x-builder, Japan';
 const email = 'x-builder@gmail.com';
-const appName = 'xPlayer -動画プレイヤー- Ver4.31.2';
+const appName = 'xPlayer -動画プレイヤー- Ver4.32.2';
 // ---------------------------------------------------------------------
 // 🔲共通変数設定🔲
 // モジュールインポート
