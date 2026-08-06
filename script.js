b0617df1d9afebec248f340e5943e6e15a99ef1f
x-builder1965 <tsuyo.hasegwa@gmail.com>
2026-08-06 10:54:29 +0900

    Ver5.00.0 開発ブランチ統合。

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------
 const copyright = 'Copyright © 2025- @x-builder, Japan';
 const email = 'x-builder@gmail.com';
-const appName = 'xPlayer -動画プレイヤー- Ver4.78.2';
+const appName = 'xPlayer -動画プレイヤー- Ver5.00.0';
 // ---------------------------------------------------------------------
 // 🔲共通変数設定🔲
 // モジュールインポート
